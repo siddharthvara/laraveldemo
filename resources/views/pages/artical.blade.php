bb942be26a10d3e70c4f061927eafd87f7b0506d
siddharth vara <siddharth.vara@gmail.com>
2018-05-23 19:56:50 +0530

    implement search

--- resources/views/pages/artical.blade.php
+++ resources/views/pages/artical.blade.php
@@ -13,13 +13,13 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <table class="table">
+<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
+    <table class="table" id = "myTable">
         <tr>
-            <td>Title</td>
-            <td>Discription</td>
-            <td></td>
-            <td></td>
+            <th>Title</th>
+            <th>Discription</th>
+            <th></th>
+            <th></th>
         </tr>
         @foreach ($flights as $artical)
         <tr>
@@ -30,6 +30,30 @@
         </tr>
         @endforeach
     </table>
+
+    <script>
+function myFunction() {
+  // Declare variables 
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
+</script>
+
 @endsection
     <!--
     </div>
