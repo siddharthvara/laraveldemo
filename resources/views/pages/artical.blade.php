706cc02cb044112ed820ce8263f359a2fbc12eed
siddharth vara <siddharth.vara@gmail.com>
2018-05-24 13:34:03 +0530

    rectify code.

--- resources/views/pages/artical.blade.php
+++ resources/views/pages/artical.blade.php
@@ -1,19 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-<body>
-<div class="container"> -->
-<?php
-    // echo '<pre>';
-    // print_r($flights);
-    // die();
-    ?>
 @extends('layouts.app')
 
 @section('content')
@@ -55,8 +39,4 @@
         @endforeach
     </table>
 @endsection
-    <!--
-    </div>
-</body>
-</html>
--->
+   
